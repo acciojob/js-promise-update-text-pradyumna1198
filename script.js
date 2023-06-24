4158b41b412886de104d4c6f2c96ad2417c7a693
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-06-24 23:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function Hello() {
+	return new Promise((resolve)=>{
+		setTimeout(()=> {
+			resolve('Hello, world!');
+		},1000);
+	});
+}
+const outElement = document.getElementById('output');
+
+Hello()
+.then((message)=> {
+	outputElement.textContent  = message;
+})
+.catch((error)=> {
+	console.log('Error',error);
+});
